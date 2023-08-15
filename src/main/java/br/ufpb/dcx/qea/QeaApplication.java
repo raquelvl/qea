224d9c42fa0ebb0dfba1e30db6225501cd62f2b9
Raquel Lopes <raquel@computacao.ufcg.edu.br>
2023-08-15 16:23:52 -0300

    Initial commit

---
+++ src/main/java/br/ufpb/dcx/qea/QeaApplication.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.qea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufpb/dcx/qea/QeaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpb.dcx.qea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
